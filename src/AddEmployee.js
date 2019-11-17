d1cad05a741c9310a7ec921ec39d3981272c4a29
KacperSlow <k.slowikowski@student.mini.pw.edu.pl>
2019-11-17 15:41:49 +0100

    Lab6Ex3 - To check

--- src/AddEmployee.js
+++ src/AddEmployee.js
@@ -3,19 +3,26 @@ class AddEmployee extends React.Component {
     constructor(props) {
         super(props);
       }
-    render(){   
+    render(){  
+        if(this.props.save){
+            return(
+                <div style={{border:'2px solid black', padding:'3px'}}>
+                    <p>Saving ...</p>
+                </div>
+            )
+        }      
     return(
         <div>
             <div style={{border:'2px solid black', padding:'3px'}}>
-                <form >
+                <form onSubmit={(e) => this.props.onSubmit(e)} >
                     <p>ID: <input disabled></input></p>
                     <p>Name: <input name="name"></input></p>
                     <p>Age: <input type="number" name="age"></input></p>
                     <p>Company: <input name="company"></input></p>
                     <p>email: <input type="email" name="email"></input></p>
                     <p>Active: <input type="checkbox" name="active"></input></p>                
-                    <button type="button" onClick={(e) => this.props.handleAddEmployee(e)}>Cancel</button>
-                    <button type="button" onClick={(e) => this.props.handleAddEmployee(e)}>Add</button>
+                    <button type="button" onClick={(e) => this.props.handleAddEmployee(e)} >Cancel</button>
+                    <button>Add</button>
                 </form>            
                 <br />
             </div>
--- src/Companies.js
+++ src/Companies.js
@@ -8,7 +8,8 @@ class Companies extends React.Component {
         this.state = {
             employees: [],
             isLoading: false,
-            add: false
+            add: false,
+            save: false
         };        
     }
 
@@ -25,21 +26,46 @@ class Companies extends React.Component {
     handleAddEmployee = (e) =>{
         this.setState({ add: !this.state.add});
     }
-
-
+    onSubmit = (e) => {
+        e.preventDefault();
+        const newEmployee = {
+          isActive: e.target.active.checked,
+          age: e.target.age.value,
+          name: e.target.name.value,
+          company: e.target.company.value,
+          email: e.target.email.value
+        }         
+        fetch('http://localhost:3000/employees', {
+        method: 'POST', 
+        headers: {
+            'Content-Type': 'application/json',
+            'Accept': 'application/json'
+        },
+        body: JSON.stringify(newEmployee)
+        })
+        .then(response => {response.json()})       
+            
+        this.setState({save: true}),
+        fetch('http://localhost:3000/employees')
+        .then(response => response.json())
+        .then(response => {
+            this.setState({ employees: response, isLoading: false, add:false, save: false })
+        })
+    }
     render() {
         if (this.state.isLoading){
             return <p>Loading ...</p>;
         }
-        if(this.state.add){
+        if(this.state.add || this.state.save) {
             return(
                 <div>
                     <button onClick={this.handleAddEmployee}>Add Employee</button>
-                    <AddEmployee handleAddEmployee={this.handleAddEmployee}/>
-                    <Employee employees={this.state.employees} add={this.state.add}/>
+                    <AddEmployee handleAddEmployee={this.handleAddEmployee} onSubmit={this.onSubmit} save={this.state.save}/>
+                    <Employee employees={this.state.employees}/>
                 </div>
             )
         }
+
 
         return (
             <div>
